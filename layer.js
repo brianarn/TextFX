45ba2e665c9ae5745209e327dcfc9af1084996f5
Brian Arnold <brianarn@gmail.com>
2010-07-26 17:59:55 -0600

    Initial commit

---
+++ layer.js
@@ -0,0 +1,7 @@
+dojo.provide("TextFX.layer");
+
+// register module path for this Demo
+dojo.registerModulePath("TextFX","../../TextFX");
+
+/* put all of the dojo.requires for your demo here */
+dojo.require("TextFX.init");
